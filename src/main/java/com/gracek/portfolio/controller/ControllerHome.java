599bf28995f31cc16ce1d5f71a41b5ba2a343f3f
root <root@GrvceLap.localdomain>
2025-06-25 16:47:18 +0200

    25/06 init portfolio

---
+++ src/main/java/com/gracek/portfolio/controller/ControllerHome.java
@@ -0,0 +1,38 @@
+package com.gracek.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerHome {
+     @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "projects";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/{path:[^\\.]*}")
+    public String fallback() {
+        return "error";
+    }
+
+    @RequestMapping("/maintenance")
+    public String maintenance() {
+        return "maintenance";
+    }
+}
